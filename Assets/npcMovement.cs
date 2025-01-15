bed6aa1208699ce8695c7588b4a9fa856a03c1a8
Averium <averym515@gmail.com>
2025-01-15 16:04:18 -0500

    Navmesh added
    -Very simple click to move in parking lot

---
+++ Assets/npcMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npcMovement : MonoBehaviour
+{
+
+    Camera mainCamera;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //Left mouse button
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                // Check if the hit point is on the NavMesh
+                if (NavMesh.SamplePosition(hit.point, out NavMeshHit navMeshHit, 1.0f, NavMesh.AllAreas))
+                {
+                    agent.SetDestination(navMeshHit.position);
+                }
+            }
+        }
+    }
+}
