900bac5ceaa545fd7878ab0566f2ff20bd7db824
Kirk Lefevre <kirklefevre@mac.lan>
2025-01-21 18:17:13 -0500

    add proper turns per role, and be able to use specific characters within each role.

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,81 +1,37 @@
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.UI;
-
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 10f; // Horizontal movement speed
+    public float moveSpeed = 10f; // Movement speed
     public float verticalSpeed = 5f; // Vertical movement speed
-    public float smoothingSpeed = 0.1f; // Determines how smooth the movement is
-
     private Vector3 moveDirection = Vector3.zero;
-    private Vector3 currentVelocity = Vector3.zero; // Used for SmoothDamp
-    public enum Roles
+
+    public RoleManager.Roles PlayerRole { get; private set; } // Player's role
+
+    private bool isControlEnabled = false; // Can the player control their character?
+
+    public void EnableControl(bool enable)
     {
-        None,
-        LawEnforcement,
-        FireDepartment,
-        OnSiteSecurity,
-        RadiationSaftey,
-        Dispatch,
-        Spectator,
-        Instructor,
+        isControlEnabled = enable;
     }
 
-    [SerializeField] private Roles playerRole;
-    public GameObject cameras;
-    public npcMovement npcs;
-    private cameraSwitch cameraswitch;
-
-    private GameObject[] moveableChars; //Array of gameobjects that this player is allowed to interact with
-    private List<GameObject> selectedChars = new List<GameObject>();
-    public PhaseManager phaseManager;
-
-    private void Start()
+    private void Update()
     {
-        cameraswitch = cameras.GetComponent<cameraSwitch>();
-    }
+        if (!isControlEnabled) return;
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0) && playerRole != Roles.None)
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (Input.GetKey(KeyCode.Space))
         {
-            Camera mainCamera = Camera.main;
-
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                GameObject hitObj = hit.collider.gameObject;
-                if (hitObj.tag == playerRole.ToString() || (playerRole == Roles.Instructor && hitObj.tag != "Untagged"))
-                {
-                    GameObject moveToolRing = hitObj.transform.GetChild(2).gameObject;
-                    moveToolRing.SetActive(true);
-                    selectedChars.Add(hitObj);
-                    return;
-                }
-
-            }
-
-            npcs.refreshCamera();
-            npcs.moveFormation(selectedChars.ToArray());
+            moveDirection.y = 1f;
         }
         if (Input.GetMouseButtonDown(1) && playerRole != Roles.None)
         {
-            Camera mainCamera = Camera.main;
-
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                GameObject hitObj = hit.collider.gameObject;
-                if (hitObj.tag == playerRole.ToString() || (playerRole == Roles.Instructor && hitObj.tag != "Untagged"))
-                {
-                    GameObject moveToolRing = hitObj.transform.GetChild(2).gameObject;
-                    moveToolRing.SetActive(false);
-                    selectedChars.Remove(hitObj);
-                    return;
-                }
-
-            }
+            moveDirection.y = -1f;
         }
         if (Input.GetKeyDown(KeyCode.Alpha1)) cameraswitch.SwitchCamera(0);
         if (Input.GetKeyDown(KeyCode.Alpha2)) cameraswitch.SwitchCamera(1);
@@ -84,105 +40,39 @@ public class playerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Alpha0) && playerRole == Roles.Instructor) // Next phase
         {
-            phaseManager.NextPhase();
+            moveDirection.y = 0f;
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha9) && playerRole == Roles.Instructor) // Previous phase
-        {
-            phaseManager.PreviousPhase();
-        }
-
-        if (Input.GetKeyDown(KeyCode.U)) // Undo last action
+        if (Input.GetKeyDown(KeyCode.Alpha3)) // Undo action
         {
             UndoLastAction();
         }
 
-
-
-
+        if (Input.GetKeyDown(KeyCode.Alpha4)) // End turn
+        {
+            EndTurn();
+        }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        // Calculate the target velocity based on moveDirection
+        if (!isControlEnabled) return;
+
         Vector3 targetVelocity = transform.TransformDirection(moveDirection) * moveSpeed;
         targetVelocity.y = moveDirection.y * verticalSpeed;
 
-        // Smoothly interpolate the current velocity towards the target velocity
-        currentVelocity = Vector3.SmoothDamp(currentVelocity, targetVelocity, ref currentVelocity, smoothingSpeed);
-
-        // Apply the smoothed velocity to the player's position
-        transform.position += currentVelocity * Time.fixedDeltaTime;
-    }
-
-    private GameObject[] GetNpcs(string role)
-    {
-        if (role != "Instructor")
-        {
-            Debug.Log("Not instructor");
-            return GameObject.FindGameObjectsWithTag(role);
-        }
-        else
-        {
-            GameObject[] Fire = GameObject.FindGameObjectsWithTag("FireDepartment");
-            GameObject[] Law = GameObject.FindGameObjectsWithTag("LawEnforcement");
-
-            List<GameObject> npcs = new List<GameObject>(Fire);
-            npcs.AddRange(Law);
-
-            return npcs.ToArray();
-        }
-
-    }
-
-    public void onButtonClick(Button button)
-    {
-
-        string npcRole = "";
-        switch (button.name)
-        {
-
-            case "LawEnfButton":
-                npcRole = "LawEnforcement";
-                playerRole = Roles.LawEnforcement;
-                break;
-            case "FireDeptButton":
-                npcRole = "FireDepartment";
-                playerRole = Roles.FireDepartment;
-                break;
-
-            case "InstructorButton":
-                npcRole = "Instructor";
-                playerRole = Roles.Instructor;
-                break;
-        }
-
-        moveableChars = GetNpcs(npcRole);
-
-        //Hide UI
-        GameObject buttonUI = button.gameObject.transform.parent.gameObject;
-        buttonUI.gameObject.SetActive(false);
-
-        //Activate Camera
-        //TODO: Enable Camera Movement only for instructor (Waiting for a more specific implementation of camera position) OwO
-        playerController playerController = transform.GetComponent<playerController>();
-        playerController.enabled = true;
-        GameObject Camera = transform.Find("Camera").gameObject;
-        CameraLook CameraLook = Camera.GetComponent<CameraLook>();
-        CameraLook.enabled = true;
+        transform.position += targetVelocity * Time.fixedDeltaTime;
     }
 
     private void UndoLastAction()
     {
-        foreach (var charObj in selectedChars)
-        {
-            Vector3 lastPosition = phaseManager.UndoAction(playerRole.ToString());
-            if (lastPosition != Vector3.zero)
-            {
-                Debug.Log($"Undo action for {charObj.name}. Moving to {lastPosition}");
-                charObj.transform.position = lastPosition;
-            }
-        }
+        Debug.Log($"{PlayerRole}: Undo last action!");
+        // This will be managed by RoleManager (future expansion)
     }
 
+    private void EndTurn()
+    {
+        Debug.Log($"{PlayerRole}: End turn!");
+        FindObjectOfType<RoleManager>().EndRoleTurn(this);
+    }
 }
