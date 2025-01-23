196ccfaefb17c7d7ea20f815d2ded6d6454e3225
Hagen Farrell <hagen.farrell@gmail.com>
2025-01-23 12:31:18 -0500

    Rough draft of custom new navmesh system

--- Assets/scripts/Pathfinder.cs
+++ Assets/scripts/Pathfinder.cs
@@ -12,12 +12,11 @@ public class Pathfinder : MonoBehaviour
 
     public List<Vector3> FindPath(Vector3 startPos, Vector3 targetPos)
     {
-        //Debug.Log(startPos + " " + targetPos);
         GridNode startNode = navMesh.GetNodeFromWorldPoint(startPos);
         GridNode targetNode = navMesh.GetNodeFromWorldPoint(targetPos);
 
-        //Debug.Log($"Start Node: {startNode?.WorldPosition} (Walkable: {startNode?.IsWalkable})");
-        //Debug.Log($"Target Node: {targetNode?.WorldPosition} (Walkable: {targetNode?.IsWalkable})");
+        Debug.Log($"Start Node: {startNode?.WorldPosition} (Walkable: {startNode?.IsWalkable})");
+        Debug.Log($"Target Node: {targetNode?.WorldPosition} (Walkable: {targetNode?.IsWalkable})");
 
         // Debug statements, needed to see if path is actually being generated.
         if (startNode == null || !startNode.IsWalkable)
