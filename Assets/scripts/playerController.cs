4c15d99f8fcdbb087b27137fe40bcccfa0180b4a
djanton1254 <dariojosetantonio@gmail.com>
2025-02-04 16:03:43 -0500

    Revert "Merge branch 'multiplayer-setup-AGAIN' of https://github.com/HagenFarrell/HospitalScenario into multiplayer-setup-AGAIN"
    This reverts commit f4783548e708df9952c7da6637058fdf865cff66, reversing
    changes made to 4ceff05431547f96f21406404f79b99981a6d5d6.

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,7 +1,4 @@
-using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class playerController : MonoBehaviour
 {
@@ -11,96 +8,29 @@ public class playerController : MonoBehaviour
 
     private Vector3 moveDirection = Vector3.zero;
     private Vector3 currentVelocity = Vector3.zero; // Used for SmoothDamp
-    public enum Roles
-    {
-        None,
-        LawEnforcement,
-        FireDepartment,
-        OnSiteSecurity,
-        RadiationSaftey,
-        Dispatch,
-        Spectator,
-        Instructor,
-    }
-
-    [SerializeField] private Roles playerRole;
-    public GameObject cameras;
-    public npcMovement npcs;
-    private cameraSwitch cameraswitch;
-
-    private GameObject[] moveableChars; //Array of gameobjects that this player is allowed to interact with
-    private List<GameObject> selectedChars = new List<GameObject>();
-    public PhaseManager phaseManager;
-
-    private void Start()
-    {
-        cameraswitch = cameras.GetComponent<cameraSwitch>();
-    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && playerRole != Roles.None)
+        // Get WASD or arrow key input for horizontal and vertical movement
+        float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrows
+        float vertical = Input.GetAxis("Vertical"); // W/S or Up/Down Arrows
+
+        // Calculate movement direction
+        moveDirection = new Vector3(horizontal, 0, vertical).normalized;
+
+        // Check for upward or downward movement with Space/Shift
+        if (Input.GetKey(KeyCode.Space))
         {
-            Camera mainCamera = Camera.main;
-
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                GameObject hitObj = hit.collider.gameObject;
-                if (hitObj.tag == playerRole.ToString() || (playerRole == Roles.Instructor && hitObj.tag != "Untagged"))
-                {
-                    GameObject moveToolRing = hitObj.transform.GetChild(2).gameObject;
-                    moveToolRing.SetActive(true);
-                    selectedChars.Add(hitObj);
-                    return;
-                }
-
-            }
-
-            npcs.refreshCamera();
-            npcs.moveNpc(selectedChars.ToArray());
+            moveDirection.y = 1f; // Move upward
         }
-        if (Input.GetMouseButtonDown(1) && playerRole != Roles.None)
+        else if (Input.GetKey(KeyCode.LeftShift))
         {
-            Camera mainCamera = Camera.main;
-
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                GameObject hitObj = hit.collider.gameObject;
-                if (hitObj.tag == playerRole.ToString() || (playerRole == Roles.Instructor && hitObj.tag != "Untagged"))
-                {
-                    GameObject moveToolRing = hitObj.transform.GetChild(2).gameObject;
-                    moveToolRing.SetActive(false);
-                    selectedChars.Remove(hitObj);
-                    return;
-                }
-
-            }
+            moveDirection.y = -1f; // Move downward
         }
-        if (Input.GetKeyDown(KeyCode.Alpha1)) cameraswitch.SwitchCamera(0);
-        if (Input.GetKeyDown(KeyCode.Alpha2)) cameraswitch.SwitchCamera(1);
-        if (Input.GetKeyDown(KeyCode.Alpha3)) cameraswitch.SwitchCamera(2);
-        if (Input.GetKeyDown(KeyCode.Alpha4)) cameraswitch.SwitchCamera(3);
-
-        if (Input.GetKeyDown(KeyCode.Alpha0) && playerRole == Roles.Instructor) // Next phase
+        else
         {
-            phaseManager.NextPhase();
+            moveDirection.y = 0f; // No vertical movement
         }
-
-        if (Input.GetKeyDown(KeyCode.Alpha9) && playerRole == Roles.Instructor) // Previous phase
-        {
-            phaseManager.PreviousPhase();
-        }
-
-        if (Input.GetKeyDown(KeyCode.U)) // Undo last action
-        {
-            UndoLastAction();
-        }
-
-
-
-
     }
 
     void FixedUpdate()
@@ -115,68 +45,4 @@ public class playerController : MonoBehaviour
         // Apply the smoothed velocity to the player's position
         transform.position += currentVelocity * Time.fixedDeltaTime;
     }
-
-    private GameObject[] GetNpcs(string role)
-    {
-        if (role != "Instructor")
-        {
-            Debug.Log("Not instructor");
-            return GameObject.FindGameObjectsWithTag(role);
-        }
-        else
-        {
-            GameObject[] Fire = GameObject.FindGameObjectsWithTag("FireDepartment");
-            GameObject[] Law = GameObject.FindGameObjectsWithTag("LawEnforcement");
-
-            List<GameObject> npcs = new List<GameObject>(Fire);
-            npcs.AddRange(Law);
-
-            return npcs.ToArray();
-        }
-
-    }
-
-    public void onButtonClick(Button button)
-    {
-
-        string npcRole = "";
-        switch (button.name)
-        {
-
-            case "LawEnfButton":
-                npcRole = "LawEnforcement";
-                playerRole = Roles.LawEnforcement;
-                break;
-            case "FireDeptButton":
-                npcRole = "FireDepartment";
-                playerRole = Roles.FireDepartment;
-                break;
-
-            case "InstructorButton":
-                npcRole = "Instructor";
-                playerRole = Roles.Instructor;
-                break;
-        }
-
-        moveableChars = GetNpcs(npcRole);
-
-        //Hide UI
-        GameObject buttonUI = button.gameObject.transform.parent.gameObject;
-        buttonUI.gameObject.SetActive(false);
-
-    }
-
-    private void UndoLastAction()
-    {
-        foreach (var charObj in selectedChars)
-        {
-            Vector3 lastPosition = phaseManager.UndoAction(playerRole.ToString());
-            if (lastPosition != Vector3.zero)
-            {
-                Debug.Log($"Undo action for {charObj.name}. Moving to {lastPosition}");
-                charObj.transform.position = lastPosition;
-            }
-        }
-    }
-
 }
