3742a83628bf466bb579d2b8bff20f56afe1d375
Kirk Lefevre <130106192+kirkcit0@users.noreply.github.com>
2025-02-17 21:01:15 -0500

    random movement works again

--- Assets/scripts/npcMovement.cs
+++ Assets/scripts/npcMovement.cs
@@ -2,19 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public static class GameObjectExtensions
-{
-    public static T GetOrAddComponent<T>(this GameObject go) where T : Component
-    {
-        T component = go.GetComponent<T>();
-        if (component == null)
-        {
-            component = go.AddComponent<T>();
-        }
-        return component;
-    }
-}
 public class npcMovement : MonoBehaviour
 {
     [SerializeField] private float stoppingRadius = 2f;
